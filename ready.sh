89d709d977d4cccc01cb2bd3f66cb07c4524569e
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-01-07 23:15:17 -0500

    Create ready.sh

---
+++ ready.sh
@@ -0,0 +1,14 @@
+    (
+      echo "# Test 1..."
+      echo "# Test 11..."
+      echo "# Test 12..."
+      echo "# Test 13..."
+      echo "# Test 14..."
+      echo "# Test 15..."
+      echo "# Test 16..."
+      echo "# Test 17..."
+      echo "# Test 18..."
+      echo "# Test 19..."
+    ) |
+zenity --progress --title "elementary OS 6.0 Dark Theme" --pulsate \
+--width 500 --text "Preparing to install elementary OS 6.0 Dark Theme..." --percentage=0
