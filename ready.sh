b93fb602af983e322dfecb609a96c7323ecca9d8
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-01-07 23:22:49 -0500

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -1,4 +1,14 @@
-    (
+# Gain admin access, required
+  while ! zenity --password  --ok-label="Continue"| sudo -S cat /dev/null >/dev/null; do
+    if $(zenity --question --icon-name="powercord" --ellipsize --text="Wrong password, would you like to cancel the installation?"); then
+      echo "no app-entry made, returning"
+      zenity --warning --ellipsize --text="The installation was interrupted."
+      return;
+    fi
+  done
+    echo "$appEntry" | sudo -S tee ${launcher}
+
+(
       echo "# Downloading..."
       sudo mkdir /usr/share/korbs-studio/
       cd /usr/share/korbs-studio/
