12b8548b985457f3487865a89ddc2d530d0d098e
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-01-07 22:38:13 -0500

    Create inter-font-elementary.sh

---
+++ inter-font-elementary.sh
@@ -0,0 +1,13 @@
+echo "# Downloading font"
+cd /usr/share/fonts
+sudo wget "https://raw.githubusercontent.com/KorbsStudio/add-new-dark-theme-to-5.1.7/master/font/inter.zip"
+echo "# Installing font"
+sudo unzip inter.zip
+echo "# Switching font"
+gsettings set org.gnome.desktop.interface document-font-name 'Inter Regular 10'
+gsettings set org.gnome.desktop.interface default-font-name 'Inter Regular 9'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Inter Bold 9'
+echo "# Cleaning" ;
+cd /usr/share/korbs-studio/
+cd /usr/share/fonts
+sudo rm inter.zip
