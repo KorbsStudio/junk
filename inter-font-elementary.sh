7634148281b26de2eb8a706a342f544885f1ab11
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2021-01-07 22:50:15 -0500

    Update inter-font-elementary.sh

--- inter-font-elementary.sh
+++ inter-font-elementary.sh
@@ -3,6 +3,7 @@ cd /usr/share/fonts
 sudo wget "https://github.com/KorbsStudio/add-new-dark-theme-to-5.1.7/raw/master/font/inter.zip"
 echo "# Installing font"
 sudo unzip inter.zip
+sudo chmod -R 777 /usr/share/fonts/truetype/inter/ # Permission issue occurs, should fix it
 echo "# Switching font"
 gsettings set org.gnome.desktop.interface document-font-name 'Inter Regular 10'
 gsettings set org.gnome.desktop.interface font-name 'Inter Regular 9'
